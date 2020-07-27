d3a34e02be4412fcecd5d9608e654c25d135f5c4
Mayuri0612 <500060750@stu.upes.ac.in>
2020-07-27 13:22:56 +0530

    Express serving static files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const http = require('http');
-
+const morgan = require('morgan')
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+//serve static file using express
+app.use(express.static(__dirname+ '/public'));
 
 //here we first setup our express server using app.use. Later we will
 //use it as parameter in http.createserver function
@@ -12,7 +16,7 @@ app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Wlcome to Express Server</h1></body><html>');
+    res.end('<html><body><h1>Welcome to Express Server</h1></body><html>');
 
 });
 
